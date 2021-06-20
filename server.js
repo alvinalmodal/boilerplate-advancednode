2276c5325183528445402f7e3a1bb4decfc16c66
alvin almodal <almodalalvin@gmail.com>
2021-06-21 07:10:43 +0800

    added  {title: 'Hello', message: 'Please login'} in serving index.pug as per requirement

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
 app.route("/").get((req, res) => {
-  res.render("index");
+  res.render("index", { title: "Hello", message: "Please login" });
 });
 
 const PORT = process.env.PORT || 3000;
